2df464738b1b403f1904f25d4c01d78bf555d254
Amirkhon-Buriev <amirkhonburiyev@gmail.com>
2024-08-05 10:49:29 +0500

    Add files via upload

---
+++ JavaSkrit.js
@@ -0,0 +1,11 @@
+let ism_keriting = prompt('ism kiriting')
+
+let tepig_keriting = prompt('tepig_keriting')
+
+let buliam_keriting = prompt('buliam_keriting')
+
+alert( ism_keriting)
+
+alert(tepig_keriting)
+
+alert(buliam_keriting)
